dce8d41aa1b7593321162cdf438b17d15530f1e2
meenal6051 <meenall6051@gmail.com>
2024-05-10 11:02:50 +0530

    copy Constructor

--- FOP/OOp's/CopyConstructor.cpp
+++ FOP/OOp's/CopyConstructor.cpp
@@ -1,9 +1,24 @@
+// Copy Constructor
+// We can Copy value from one object to another object
+// We have two objects (B1 and B2) under B1 we have 2 variables like a=90 & b=78;
+// So B2 = B1   by using this B1 object value copy to B2.
+
 #include<iostream>
 using namespace std;
 
-class Test{
+class A{
     public:
-    Test(){
-        
+    int x;
+    A(int a){                            // Parameterized Constructor
+        x=a;
     }
+    A(A &i){                            // Copy Constructor
+        x = i.x;
+    }
+};
+int main(){
+    A a1(20);                           // Calling the Parameterized Constructor
+    A a2(a1);                           // Calling the Copy Constructor
+    cout<<a2.x;
+    return 0;
 }
