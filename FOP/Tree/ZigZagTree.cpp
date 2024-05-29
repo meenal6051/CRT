e58c17bad2ee3a29c6f8df8d8fe8ce0691501535
meenal6051 <meenall6051@gmail.com>
2024-05-29 22:35:48 +0530

    ZigZag traversal to nodes of tree

--- FOP/Tree/ZigZagTree.cpp
+++ FOP/Tree/ZigZagTree.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<deque>
 using namespace std;
 
 class Node
@@ -17,8 +18,48 @@ Node* newNode(int item)
     return temp;
 }
 
-//classes and struct m access specifier is the difference 
-//struct k acess kr skte h khi bhi 
+void ZigZagTraversal(Node *root){
+    if(root == NULL)
+        return;
+    
+    deque<Node*> nodes; // Deque to store nodes
+    nodes.push_back(root); // Push root node
+    
+    // Keep track of the current level
+    bool leftToRight = true;
+    
+    while(!nodes.empty()) {
+        int levelSize = nodes.size(); // Number of nodes in the current level
+        
+        for(int i = 0; i < levelSize; ++i) {
+            if(leftToRight) {
+                // Process nodes from left to right
+                Node *temp = nodes.front();
+                nodes.pop_front();
+                cout << temp->data << " ";
+                
+                // Push children of the current node to the back of the deque
+                if(temp->left)
+                    nodes.push_back(temp->left);
+                if(temp->right)
+                    nodes.push_back(temp->right);
+            } else {
+                // Process nodes from right to left
+                Node *temp = nodes.back();
+                nodes.pop_back();
+                cout << temp->data << " ";
+                
+                // Push children of the current node to the front of the deque
+                if(temp->right)
+                    nodes.push_front(temp->right);
+                if(temp->left)
+                    nodes.push_front(temp->left);
+            }
+        }
+        
+        leftToRight = !leftToRight; // Toggle the direction for the next level
+    }
+}
 
 Node *dummyTree()
 {
@@ -36,6 +77,12 @@ Node *dummyTree()
 int main()
 {
     Node *root = dummyTree();
-    cout<<root->data<<endl;
+    // cout<<root->data<<endl;
+
+    cout << "ZigZag Traversal: ";
+    ZigZagTraversal(root);
+    cout << endl;
+    
+    return 0;
 }
 
--- FOP/Tree/tempCodeRunnerFile.cpp
+++
@@ -1,1 +0,0 @@
- cout << currentNode->data << " ";
