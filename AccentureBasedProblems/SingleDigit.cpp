94cc7175b6ac581d1b1c54db100d926124e39076
meenal6051 <meenall6051@gmail.com>
2024-09-11 01:16:23 +0530

    little bit change

--- AccentureBasedProblems/SingleDigit.cpp
+++ AccentureBasedProblems/SingleDigit.cpp
@@ -1,3 +1,11 @@
+// Problem statement :
+// Given a number  N your task is to make N a single digit by performing the following operations:
+//  ->If N is  ODD make it floor (N/2).
+//  ->If n iş already a single digit print it as it is
+//  ->If n is even make it floor((n-2)/2)
+
+// Example:Input :10
+// Output: 4
 #include<iostream>
 using namespace std;
 
